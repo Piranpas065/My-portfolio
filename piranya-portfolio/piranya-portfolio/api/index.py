46770c9d091bd94c8bb68a87b7230f125cb9057b
Piranya Bas <piranya105@gmail.com>
2026-04-04 20:19:34 +0530

    Update index.py

--- piranya-portfolio/piranya-portfolio/api/index.py
+++ piranya-portfolio/piranya-portfolio/api/index.py
@@ -1,6 +1,7 @@
 import sys
 sys.path.append('..')
-from app import app as application
+from app import app
+application = app
 
 # For Vercel Python runtime compatibility
 if __name__ == "__main__":
