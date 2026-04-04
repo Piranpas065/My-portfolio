2fd6542207de40eacadbb7e46001602d2747480c
Piranya Bas <piranya105@gmail.com>
2026-04-04 20:08:44 +0530

    Update app.py

--- piranya-portfolio/piranya-portfolio/app.py
+++ piranya-portfolio/piranya-portfolio/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 @app.route("/")
 def index():
